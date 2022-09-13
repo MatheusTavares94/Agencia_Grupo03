71b44c17fadf4b8f3b1d354582963701b330189e
MatheusTavares94 <mmatheus_ttavares@hotmail.com>
2022-09-13 20:16:38 -0300

    Testes

--- Agencia03/src/servicos/ContaCorrente.java
+++ Agencia03/src/servicos/ContaCorrente.java
@@ -4,8 +4,10 @@ public class ContaCorrente extends Conta{
 
 	public ContaCorrente(String cpfTitular, double saldo, String idAgencia, String tipo) {
 		super(cpfTitular, saldo, idAgencia,tipo);
+		this.setTipo("CONTACORRENTE");
 	}
 	public ContaCorrente() {
 		super();
+		this.setTipo("CONTACORRENTE");
 	}
 }
