b87cf65448edaab10af35d1bbe565983510d5573
MatheusTavares94 <mmatheus_ttavares@hotmail.com>
2022-09-15 19:00:49 -0300

    Buffered Reader e criação de mapas

---
+++ Agencia03/src/Mapas/MapaPessoas.java
@@ -0,0 +1,64 @@
+package Mapas;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import Enums.pessoasEnum;
+import pessoal.Cliente;
+import pessoal.Diretor;
+import pessoal.Gerente;
+import pessoal.Pessoa;
+import pessoal.Presidente;
+
+public class MapaPessoas {
+	
+	public static void main(String[] args) {
+		
+		try (BufferedReader ler = new BufferedReader(new FileReader("MapaPessoas.txt"))) {
+			String linha;
+
+			Map<String, Pessoa> mapa = new HashMap<>();
+
+			while ((linha = ler.readLine()) != null && !(linha.isEmpty())) {				
+
+				String[] s = linha.split(",");
+
+				String nome = s[0];
+				String cpf = s[1];
+				String senha = s[2];				
+				String cargo = s[3];
+				String idAgencia = s[4];
+
+				if (cargo.equalsIgnoreCase(pessoasEnum.CLIENTE.name())) {
+					Cliente cliente = new Cliente(nome, cpf, senha, cargo, idAgencia);
+					mapa.put(cpf, cliente);
+					System.out.println(cliente.getNome());
+				}
+				else if (cargo.equalsIgnoreCase(pessoasEnum.DIRETOR.name())) {
+					Diretor diretor = new Diretor(nome, cpf, senha, cargo, idAgencia);
+					mapa.put(cpf, diretor);									
+				}
+				else if (cargo.equalsIgnoreCase(pessoasEnum.GERENTE.name())) {
+					Gerente gerente = new Gerente(nome, cpf, senha, cargo, idAgencia);
+					mapa.put(cpf, gerente);									
+				}
+				else if (cargo.equalsIgnoreCase(pessoasEnum.PRESIDENTE.name())) {
+					Presidente presidente = new Presidente(nome, cpf, senha, cargo, idAgencia);
+					mapa.put(cpf, presidente);
+					System.out.println(presidente.getNome()+" cargo "+presidente.getCargo());
+				}				
+			}
+			ler.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	} 
+}
