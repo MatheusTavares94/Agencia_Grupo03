8fd64e4fc6fe42308edb7136f69041f80ad491dd
MatheusTavares94 <mmatheus_ttavares@hotmail.com>
2022-09-16 15:12:55 -0300

    Usuario e Conta a serem parametrizados

--- Agencia03/src/servicos/ContaCorrente.java
+++ Agencia03/src/servicos/ContaCorrente.java
@@ -4,10 +4,10 @@ public class ContaCorrente extends Conta{
 
 	public ContaCorrente(String cpfTitular, double saldo, String idAgencia, String tipo) {
 		super(cpfTitular, saldo, idAgencia,tipo);
-		this.setTipo("CONTACORRENTE");
+		this.setTipo("CORRENTE");
 	}
 	public ContaCorrente() {
 		super();
-		this.setTipo("CONTACORRENTE");
+		this.setTipo("CORRENTE");
 	}
 }
